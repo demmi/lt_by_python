b6fe87e7b085d28b3a26788a5f7a9a4462a96b47
Lipatnikova <aerovaya@gmail.com>
2022-09-27 08:37:34 +0400

    ООП,  Animal ( Dog, Cat)

--- Lipatnikova/hw_5.py
+++ Lipatnikova/hw_5.py
@@ -1,40 +1,35 @@
 class Animal:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
-        self.weight = 0
+        self.age = age
 
     def speak(self):
         pass
 
 
 class Dog(Animal):
-    def __init__(self, name):
-        super(Dog, self).__init__(name)
-        self.weight = 10
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
 
     def speak(self):
         print("woof")
 
 
 class Cat(Animal):
-    def __init__(self, name):
-        super(Cat, self).__init__(name)
-        self.weight = 4
+    def __init__(self, name, age, food):
+        super(Cat, self).__init__(name, age)
+        self.food = food
 
     def speak(self):
         print("meow")
 
 
-
-animal = Animal("animal")
-animal.speak()
-print(animal.weight)
-
-dog = Dog("dog")
+dog = Dog("Tom", 1, 'black')
 dog.speak()
-print(dog.weight)
+print(f'My name is {dog.name}, my age: {dog.age}, my color is {dog.color}')
 
-cat = Cat("cat")
+cat = Cat("Den", 2, 'wiskas')
 cat.speak()
-print(cat.weight)
+print(f'My name is {cat.name}, my age: {cat.age}, I love to eat {cat.food}')
 
