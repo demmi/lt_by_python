5f83669b87714924091ef5fca0e5163d778d9fb9
Andrew Gerasimchik <gerasimchick@tut.by>
2022-09-27 13:00:16 +0300

    hw4 my_calc module test

--- AndreyHerasimchyk/HW/test/test_hw4.py
+++ AndreyHerasimchyk/HW/test/test_hw4.py
@@ -1,4 +1,7 @@
+import pytest
+
 from AndreyHerasimchyk.HW.hw4 import square, only_positive, mult_positive, print_kwargs
+from AndreyHerasimchyk.HW.my_calc import sum_, sub_, mult_, div_
 
 
 def test_square():
@@ -14,4 +17,31 @@ def test_mult_positive():
 
 
 def test_print_kwargs():
-    assert print_kwargs(name="John", last_name="Silver", age=35, position="Python developer") == 'name: John\nlast_name: Silver\nage: 35\nposition: Python developer\n'
+    assert print_kwargs(name="John", last_name="Silver", age=35,
+                        position="Python developer") == 'name: John\nlast_name: Silver\nage: 35\nposition: Python developer\n'
+
+
+cases = ((5, 10), (-1, 7), (0, 15), (15, 0), (-100, -55), (11, 4), (23, -12))
+
+
+@pytest.mark.parametrize('a, b', cases)
+def test_sum_(a, b):
+    assert sum_(a, b) == a + b
+
+
+@pytest.mark.parametrize('a, b', cases)
+def test_sub_(a, b):
+    assert sub_(a, b) == a - b
+
+
+@pytest.mark.parametrize('a, b', cases)
+def test_mult_(a, b):
+    assert mult_(a, b) == a * b
+
+
+@pytest.mark.parametrize('a, b', cases)
+def test_div_(a, b):
+    if b:
+        assert div_(a, b) == a / b
+    else:
+        assert div_(a, b) == 'На ноль делить нельзя'
