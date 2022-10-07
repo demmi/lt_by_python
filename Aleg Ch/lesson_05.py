ba1bbe622d302c21123c81beff7d912d90930e6f
Aleg Ch <antidominanta@gmail.com>
2022-10-07 12:01:51 +0300

    lesson_05 26:50

--- Aleg Ch/lesson_05.py
+++ Aleg Ch/lesson_05.py
@@ -1,2 +1,34 @@
 # OOP. GIT. GITHUB
-# ///// 0:07
+# ----- Создаем класс Employee с атрибутами (параметрами) name, surname
+# class Employee:
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#
+# employee1 = Employee('Alex', 'Smith')  # Создаем объект класса Employee с конкретными аргументами
+# print(employee1.name)  # Alex
+# print(employee1.surname)  # Smith
+#
+# employee2 = Employee(surname='Anders', name='Nika')
+# print(employee2.name)  # Nika
+# print(employee2.surname)  # Anders
+#
+# ----- Принцип наследования
+class Employee:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def walk(self):  # Создаем метод (функцию, действие) класса
+        return 'I can walk!'
+
+employee1 = Employee('Alex', 'Smith')
+print(employee1.walk())  # I can walk!
+
+class Developer(Employee):  # Создаем подкласс Developer в классе Employee
+
+
+dev1 =
+
+///// 26:50
