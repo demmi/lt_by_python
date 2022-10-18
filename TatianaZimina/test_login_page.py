53914c6a6eb9f6b02d4a7a61e3df1868c9e2b23b
LENOVOTT\tatya <t.trushlyakova@gmail.com>
2022-10-18 11:09:59 -0700

    lesson 6.1

---
+++ TatianaZimina/test_login_page.py
@@ -0,0 +1,26 @@
+import time
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://www.saucedemo.com/')
+
+
+def test_title():
+    title = driver.title
+    assert title == 'Swag Labs'
+
+
+def test_login():
+    time.sleep(1)
+    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+    driver.find_element(By.ID, 'password').send_keys('secret_sauce')
+    driver.find_element(By.ID, 'login-button').click()
+    time.sleep(1)
+
+    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'wrong url'
+
+    driver.quit()
