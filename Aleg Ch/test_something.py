cccd018efc9923259eb9fa89a3081400247b896b
Aleg Ch <antidominanta@gmail.com>
2022-10-25 20:09:52 +0300

    lesson 06 install pytest 1:25:55

--- Aleg Ch/test_something.py
+++ Aleg Ch/test_something.py
@@ -1,13 +1,14 @@
-from selenium.webdriver.common.by import By
 import time
-from webbrowser import BaseBrowser
-
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.get("web page URL")
 driver.get("http://suninjuly.github.io/cats.html")
 
-# ////////// 1:15:06
+time.sleep(5)
+bullet_cat=driver.find_element(By.XPATH, "/html/body/main/div/div/div/div[1]/div/div/div/div/button[1]").click()
+time.sleep(5)
+
+
+# ////////// 1:25:55
