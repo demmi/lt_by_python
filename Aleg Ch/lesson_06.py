6f236cac4e957dc4a4baa35631b72ab8c65b041e
Aleg Ch <antidominanta@gmail.com>
2022-10-26 11:45:55 +0300

    lesson 06 mytest_01

---
+++ Aleg Ch/lesson_06.py
@@ -0,0 +1,27 @@
+# Lesson 06 html, css-selectors, xpath
+# ----- Поиск элементов -----
+import time
+# import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://suninjuly.github.io/cats.html")  # открыть сайт, страницу cats
+time.sleep(5)  # задержка 5 сек
+
+# --- найдем картинку кота по имени bullet и нажмем под ней кнопку
+bullet_cat=driver.find_element(By.XPATH, "/html/body/main/div/div/div/div[1]/div/div/div/div/button[1]").click()
+time.sleep(5)
+
+# # --- протестируем текст 'Bullet cat'
+# создаем test_mytest01.py и дальше работаем в нем
+# вписать там: import pytest
+# запустить test_mytest01 с функцией:
+# def test_1():
+#     bullet_cat_text = driver.find_element(By.XPATH, '//p[text()="Bullet cat"]').text
+#     assert bullet_cat_text == 'Bullet cat', 'wrong!'
+#     time.sleep(5)
+
+
+# ////////// 1:25:55
---
+++ Aleg Ch/test_mytest01.py
@@ -0,0 +1,34 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://suninjuly.github.io/cats.html")  # открыть сайт, страницу cats
+time.sleep(3)  # задержка 3 сек
+
+# # --- найдем картинку кота по имени bullet и нажмем под ней кнопку
+# bullet_cat=driver.find_element(By.XPATH, "/html/body/main/div/div/div/div[1]/div/div/div/div/button[1]").click()
+# time.sleep(5)
+
+
+# # --- протестируем текст 'Bullet cat'
+def test_1():
+    bullet_cat_text = driver.find_element(By.XPATH, '//p[text()="Bullet cat"]').text
+    assert bullet_cat_text == 'Bullet cat', 'wrong!'
+    time.sleep(5)
+# 1 passed, 1 warning in 43.25s
+
+
+# # --- протестируем негативный текст, с ошибкой 'Bulletcat'
+def test_2():
+    bullet_cat_text = driver.find_element(By.XPATH, '//p[text()="Bullet cat"]').text
+    assert bullet_cat_text == 'Bulletcat', 'wrong!'
+    time.sleep(5)
+# test_mytest01.py::test_2 FAILED                                          [100%]
+# test_mytest01.py:24 (test_2)
+# 'Bullet cat' != 'Bulletcat'
+#
+# Expected :'Bulletcat'
+# Actual   :'Bullet cat'
